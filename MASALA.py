307142d464a282511011be4319026c0df96ee876
Yusufbek0606 <117284465+Yusufbek0606@users.noreply.github.com>
2022-11-02 18:25:35 +0500

    Add files via upload

---
+++ MASALA.py
@@ -0,0 +1,107 @@
+#a=4
+#p=a*4
+#print(p)
+#_________________
+
+#a=5
+#b=7
+#S=a*b
+#P=2*(a+b)
+#print(S,P)
+#_____________________
+
+#R=5
+#p=3.14
+#L=2*p*R 
+#S=p*R*R
+#print(L,S)
+#____________
+
+
+
+#L=100
+#b=L/100
+#print(b)
+
+
+
+
+
+
+#t=123
+#a=t//100
+#b=(t//10)%10
+#c=t%10
+#result=b*100+a*10+c
+#print(result)
+
+from unittest import result
+
+
+#d=123
+#a=d//100
+#b=(d//10)%10
+#c=d%10
+#if a<b and b<c and c>a :
+ #print(True)
+#else:
+ #   print(False)
+
+
+
+
+
+
+
+
+
+#result=b*1000+e*100+c*10+a
+#print(result)
+#________________________________
+#a=int(input('1-sonni kiriting='))
+#b=int(input('2-sonni kiriting='))
+#d=input('kerakli amalni kiriting=')
+#if d=='^': print(a**b)
+#elif d=='-': print(a-b)
+#elif d=='*': print(a*b)
+#elif d=='+': print(a+b)
+#elif d=='/': print(a/b)
+
+#____________________
+
+#m=int(input('yil oyining raqamini kiriting:'))
+#if m==2: d=28
+#elif m in (1,2,3,4,5,6,7,8,9,10,11,12): d=31
+#else: d=30
+#print(m,"-oy",d,"kundan iborat")
+ #________________-________-
+ 
+#for i in 'salom dunyo':
+ #   print(i*4,end='')
+# __________________
+
+#a=['Mariyaning','kichina','qozichogi','bor']
+#for i in range(len(a)):
+ #   print (i,a[i])
+ #__________________
+
+#for i in "helloworld":
+    #if i=='r':
+    #    break 
+   # print(i*2, end='')
+
+#____________________
+
+
+
+#for i in "hello world":
+    #if i=='o':
+    #    continue
+   # print(i*2, end='')
+#____________________________________________
+
+©j
+a=17
+b=3
+s=a/b
+print(s)
